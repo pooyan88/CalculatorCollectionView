734066e8525ccff24e27e0d1d827747696664010
Pooyan J <pooyanjavidi@gmail.com>
2025-02-18 10:17:20 +0330

    first commit

---
+++ Calculator/CollectionViewCell/CalculatorCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorCollectionViewCell.swift
+//  Calculator
+//
+//  Created by Pooyan J on 11/28/1403 AP.
+//
+
+import UIKit
+
+class calculatorCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
